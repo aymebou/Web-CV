1063d7a05de2c63895edde0f644b528b6d2e61fc
aymebou <ayme-bou@hotmail.fr>
2019-01-10 17:56:54 +0100

    better router management

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,24 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, AfterViewChecked} from '@angular/core';
+import { Location } from '@angular/common';
+import {Router} from "@angular/router";
+
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements  AfterViewChecked
+ {
 
   topbarTitle: string;
   topbarClass: string;
+  route: string;
 
 
 
 
-  constructor() {
-    this.topbarTitle= "Aymeric Bouzigues";
-    this.topbarClass = 'Regular'
+
+  constructor(private location: Location, private router: Router) {
+    router.events.subscribe((val) => {
+        this.initTopBarClass(location.path());
+    });
   }
 
-  ngOnInit() {
+  ngAfterViewChecked() {
+  }
+
+  initTopBarClass = function(url: any) {
+    if (url != "/overview") {
+      this.topbarTitle= "Aymeric Bouzigues";
+      this.topbarClass = 'Regular'
+    } else {
+      this.topbarTitle= "Overview";
+      this.topbarClass = 'Overview'
+    }
   }
 
   adaptTopBar = function(colorClass: string, title: string) {
